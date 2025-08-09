c83fef389dbcb79f50bb63ea62773d37f2822f08
DanielOgnyanov <escape69@abv.bg>
2025-08-09 05:17:17 +0300

    create all the repos init

---
+++ src/main/java/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+
+import models.entities.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, Long>  {
+}
---
+++ src/main/java/repository/StockRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+
+import models.entities.StockEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository  extends JpaRepository<StockEntity, Long> {
+}

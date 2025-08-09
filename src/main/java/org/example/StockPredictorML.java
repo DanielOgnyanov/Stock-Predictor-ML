8edd7f323980ab53fba00df75aa49627ae039500
DanielOgnyanov <escape69@abv.bg>
2025-08-09 06:06:18 +0300

    main initial setup

---
+++ src/main/java/org/example/StockPredictorML.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPredictorML {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockPredictorML.class, args);
+    }
+}

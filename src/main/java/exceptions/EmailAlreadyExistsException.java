f0e87659e94384c51746d71c0be539c2c33e1c86
DanielOgnyanov <escape69@abv.bg>
2025-08-13 07:11:44 +0300

    global exceptions

---
+++ src/main/java/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package main.java.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("Email already registered: " + email);
+    }
+}
---
+++ src/main/java/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package main.java.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String email) {
+        super("User not found with email: " + email);
+    }
+}

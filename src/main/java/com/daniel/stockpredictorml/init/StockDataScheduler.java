f469e5a8814d9102c03f7ed94b590f759023b2ff
DanielOgnyanov <escape69@abv.bg>
2025-08-14 13:20:52 +0300

    setup a Event listener to call the api every 40 min

--- src/main/java/com/daniel/stockpredictorml/init/StockDataScheduler.java
+++ src/main/java/com/daniel/stockpredictorml/init/StockDataScheduler.java
@@ -1,19 +1,33 @@
 package com.daniel.stockpredictorml.init;
 
-
 import com.daniel.stockpredictorml.service.StockService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StockDataScheduler {
 
     private final StockService stockService;
+    private boolean initialRunDone = false;
 
     public StockDataScheduler(StockService stockService) {
         this.stockService = stockService;
     }
 
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        stockService.fetchAndStoreQuotes();
+        initialRunDone = true;
+    }
+
+
+    @Scheduled(fixedRate = 40 * 60 * 1000)
+    public void scheduledFetch() {
+        if (initialRunDone) {
+            stockService.fetchAndStoreQuotes();
+        }
+    }
 }

adef25152f193444310956d4c34619ed5056af11
DanielOgnyanov <escape69@abv.bg>
2025-08-16 07:42:03 +0300

    change the fields inside the dto and entity

--- src/main/java/com/daniel/stockpredictorml/models/dto/StockQuoteResponseDTO.java
+++ src/main/java/com/daniel/stockpredictorml/models/dto/StockQuoteResponseDTO.java
@@ -1,6 +1,7 @@
 package com.daniel.stockpredictorml.models.dto;
 
-
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,39 +9,20 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class StockQuoteResponseDTO {
 
-    private String symbol;
-    private String name;
-    private String exchange;
-    private String micCode;
-    private String currency;
-
-    private Instant datetime;
-    private Instant lastQuoteAt;
-
-    private BigDecimal open;
-    private BigDecimal high;
-    private BigDecimal low;
-    private BigDecimal close;
-
-    private Long volume;
-    private BigDecimal previousClose;
-    private BigDecimal changeValue;
-    private BigDecimal percentChange;
-    private Long averageVolume;
-
-    private Boolean isMarketOpen;
-
-    private BigDecimal extendedChange;
-    private BigDecimal extendedPercentChange;
-    private BigDecimal extendedPrice;
-    private Instant extendedTimestamp;
-
-
+   private String symbol;
+   private String name;
+   private String currency;
+   private double open;
+   private double high;
+   private double low;
+   private double close;
+   private int volume;
 }
--- src/main/java/com/daniel/stockpredictorml/models/entities/StockEntity.java
+++ src/main/java/com/daniel/stockpredictorml/models/entities/StockEntity.java
@@ -1,6 +1,5 @@
 package com.daniel.stockpredictorml.models.entities;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
@@ -13,17 +12,17 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "stocks", indexes = {
-        @Index(name = "idx_symbol", columnList = "symbol"),
-        @Index(name = "idx_datetime", columnList = "datetime")
+        @Index(name = "idx_symbol", columnList = "symbol")
 })
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class StockEntity extends BaseEntity{
+public class StockEntity extends BaseEntity {
 
     @NotNull(message = "Symbol is required")
     @Size(max = 10, message = "Symbol max length is 10")
@@ -34,73 +33,27 @@ public class StockEntity extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    @Size(max = 10, message = "Exchange max length is 10")
-    @Column(name = "exchange")
-    private String exchange;
-
-    @Size(max = 10, message = "MIC code max length is 10")
-    @Column(name = "mic_code")
-    private String micCode;
-
     @Size(max = 5, message = "Currency max length is 5")
     @Column(name = "currency")
     private String currency;
 
-    @NotNull(message = "Datetime is required")
-    @Column(name = "datetime", nullable = false)
-    private Instant datetime;
-
-    @Column(name = "last_quote_at")
-    private Instant lastQuoteAt;
-
     @DecimalMin(value = "0.0", inclusive = false, message = "Open price must be positive")
     @Column(name = "open", precision = 19, scale = 6)
-    private BigDecimal open;
+    private double open;
 
     @DecimalMin(value = "0.0", inclusive = false, message = "High price must be positive")
     @Column(name = "high", precision = 19, scale = 6)
-    private BigDecimal high;
+    private double high;
 
     @DecimalMin(value = "0.0", inclusive = false, message = "Low price must be positive")
     @Column(name = "low", precision = 19, scale = 6)
-    private BigDecimal low;
+    private double low;
 
     @DecimalMin(value = "0.0", inclusive = false, message = "Close price must be positive")
     @Column(name = "close", precision = 19, scale = 6)
-    private BigDecimal close;
+    private double close;
 
     @PositiveOrZero(message = "Volume cannot be negative")
     @Column(name = "volume")
-    private Long volume;
-
-    @DecimalMin(value = "0.0", inclusive = false, message = "Previous close price must be positive")
-    @Column(name = "previous_close", precision = 19, scale = 6)
-    private BigDecimal previousClose;
-
-    @Column(name = "change_value", precision = 19, scale = 6)
-    private BigDecimal changeValue;
-
-    @DecimalMin(value = "-100.0", message = "Percent change cannot be less than -100%")
-    @DecimalMax(value = "100.0", message = "Percent change cannot be greater than 100%")
-    @Column(name = "percent_change", precision = 8, scale = 4)
-    private BigDecimal percentChange;
-
-    @PositiveOrZero(message = "Average volume cannot be negative")
-    @Column(name = "average_volume")
-    private Long averageVolume;
-
-    @Column(name = "is_market_open")
-    private Boolean isMarketOpen;
-
-    @Column(name = "extended_change", precision = 19, scale = 6)
-    private BigDecimal extendedChange;
-
-    @Column(name = "extended_percent_change", precision = 8, scale = 4)
-    private BigDecimal extendedPercentChange;
-
-    @Column(name = "extended_price", precision = 19, scale = 6)
-    private BigDecimal extendedPrice;
-
-    @Column(name = "extended_timestamp")
-    private Instant extendedTimestamp;
+    private int volume;
 }

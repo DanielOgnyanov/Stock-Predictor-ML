db55585094ac6829689ad6a35c0ce094b523eb38
DanielOgnyanov <escape69@abv.bg>
2025-08-18 06:44:22 +0300

    update pom and main

--- src/main/java/com/daniel/stockpredictorml/StockPredictorML.java
+++ src/main/java/com/daniel/stockpredictorml/StockPredictorML.java
@@ -2,8 +2,11 @@ package com.daniel.stockpredictorml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.daniel.stockpredictorml")
+@SpringBootApplication
+
 public class StockPredictorML {
 
     public static void main(String[] args) {

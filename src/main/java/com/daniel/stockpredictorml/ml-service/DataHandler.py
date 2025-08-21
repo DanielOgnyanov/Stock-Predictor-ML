bb1743885aecef71f63369aabfa7d8835c46a27d
DanielOgnyanov <escape69@abv.bg>
2025-08-21 10:56:04 +0300

    data handler def get_features_and_target separate the numeric and non numeric raw data and set the close as y for the stock model

--- src/main/java/com/daniel/stockpredictorml/ml-service/DataHandler.py
+++ src/main/java/com/daniel/stockpredictorml/ml-service/DataHandler.py
@@ -13,3 +13,10 @@ class DataHandler:
         self.df = pd.DataFrame(self.data)
         return self.df
 
+    def get_features_and_target(self, target_column="close"):
+
+        if self.df is None:
+            self.to_dataframe()
+        X = self.df.drop(columns=[target_column, "symbol", "name", "currency"])  # only numeric features
+        y = self.df[target_column]
+        return X, y

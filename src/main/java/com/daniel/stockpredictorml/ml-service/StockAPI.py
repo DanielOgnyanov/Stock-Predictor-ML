ff6482103290bd37dfd3ea23afae3dea66e49d37
DanielOgnyanov <escape69@abv.bg>
2025-08-22 08:37:33 +0300

    Modify the stockapi for automatic train if the model is not trained and return only the symbol and the prediction as json response.

--- src/main/java/com/daniel/stockpredictorml/ml-service/StockAPI.py
+++ src/main/java/com/daniel/stockpredictorml/ml-service/StockAPI.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from StockModel import StockModel
+from DataHandler import DataHandler
 
 class StockAPI:
     def __init__(self):
@@ -14,17 +15,33 @@ class StockAPI:
         def ping():
             return "Stock API is running!"
 
-
         @self.app.route('/predict', methods=['POST'])
         def predict():
             data = request.json
-            df = pd.DataFrame(data)
-            predictions = self.model.predict(df)
-            return jsonify(predictions.tolist())
+            handler = DataHandler(data)
+            X, y = handler.get_features_and_target(target_column="close")
+
+
+            if self.model.model is None:
+                self.model.train(X, y)
+
+
+            predictions = self.model.predict(X)
+
+
+            response = []
+            for i, row in enumerate(data):
+                response.append({
+                    "symbol": row["symbol"],
+                    "predicted_close": float(predictions[i])
+                })
+
+            return jsonify(response)
 
     def run(self):
         self.app.run(debug=True)
 
-        if __name__ == "__main__":
-            api = StockAPI()
-            api.run()
+
+if __name__ == "__main__":
+    api = StockAPI()
+    api.run()

c97dc633933198b9259b8cf27b306fc7e1918eea
DanielOgnyanov <escape69@abv.bg>
2025-08-23 09:42:47 +0300

    stock repo method to find stock by symbol

--- src/main/java/com/daniel/stockpredictorml/repository/StockRepository.java
+++ src/main/java/com/daniel/stockpredictorml/repository/StockRepository.java
@@ -7,6 +7,13 @@ import com.daniel.stockpredictorml.models.entities.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StockRepository  extends JpaRepository<StockEntity, Long> {
+
+    Optional<StockEntity> findBySymbol(String symbol);
+
+    List<StockEntity> findAllBySymbol(String symbol);
 }

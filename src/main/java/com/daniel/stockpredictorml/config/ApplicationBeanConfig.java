841bd7e09d19f05183d0ff28f96e4d37a372d5f8
DanielOgnyanov <escape69@abv.bg>
2025-08-24 08:47:46 +0300

    add rest template as bean inside config

--- src/main/java/com/daniel/stockpredictorml/config/ApplicationBeanConfig.java
+++ src/main/java/com/daniel/stockpredictorml/config/ApplicationBeanConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationBeanConfig {
@@ -20,4 +21,10 @@ public class ApplicationBeanConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }

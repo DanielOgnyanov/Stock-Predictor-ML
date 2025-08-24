8c29c77fc5d8e9127dd42773382d021207aaaca9
DanielOgnyanov <escape69@abv.bg>
2025-08-24 08:59:44 +0300

    Predict Request dto

--- src/main/java/com/daniel/stockpredictorml/models/dto/PredictRequestDTO.java
+++ src/main/java/com/daniel/stockpredictorml/models/dto/PredictRequestDTO.java
@@ -1,9 +1,13 @@
 package com.daniel.stockpredictorml.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class PredictRequest {
+@NoArgsConstructor
+@AllArgsConstructor
+public class PredictRequestDTO {
     private String symbol;
     private Double open;
     private Double high;

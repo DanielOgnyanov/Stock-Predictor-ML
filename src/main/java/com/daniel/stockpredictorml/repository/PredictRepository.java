65427950a9644f2252e7f86aba22fb94d5bf71f9
DanielOgnyanov <escape69@abv.bg>
2025-08-25 09:34:07 +0300

    find predicted symbol in db query

--- src/main/java/com/daniel/stockpredictorml/repository/PredictRepository.java
+++ src/main/java/com/daniel/stockpredictorml/repository/PredictRepository.java
@@ -5,6 +5,10 @@ import com.daniel.stockpredictorml.models.entities.PredictEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PredictRepository  extends JpaRepository<PredictEntity, Long> {
+
+    Optional<PredictEntity> findTopBySymbolOrderByUpdatedAtDesc(String symbol);
 }

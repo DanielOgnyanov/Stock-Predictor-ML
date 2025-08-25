c3d1c7fddaddde0f1b4dd4824c27f6bb0239e20f
DanielOgnyanov <escape69@abv.bg>
2025-08-25 09:35:04 +0300

    getLatestPrediction method

--- src/main/java/com/daniel/stockpredictorml/service/PredictService.java
+++ src/main/java/com/daniel/stockpredictorml/service/PredictService.java
@@ -1,6 +1,11 @@
 package com.daniel.stockpredictorml.service;
 
+import com.daniel.stockpredictorml.models.dto.PredictResponseDTO;
+
+import java.util.Optional;
+
 public interface PredictService {
 
     void predictAndSave(String symbol);
+    Optional<PredictResponseDTO> getLatestPrediction(String symbol);
 }

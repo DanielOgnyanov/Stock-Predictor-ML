3e03de09c5624463ac42a14401faa9d0dc1ea3be
DanielOgnyanov <escape69@abv.bg>
2025-08-25 15:27:00 +0300

    post request to predict and save the data in db

--- src/main/java/com/daniel/stockpredictorml/web/PredictRestController.java
+++ src/main/java/com/daniel/stockpredictorml/web/PredictRestController.java
@@ -3,21 +3,34 @@ package com.daniel.stockpredictorml.web;
 
 import com.daniel.stockpredictorml.models.dto.PredictResponseDTO;
 import com.daniel.stockpredictorml.service.PredictService;
+import com.daniel.stockpredictorml.service.StockService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/predict")
 public class PredictRestController {
 
     private final PredictService predictService;
+    private final StockService stockService;
 
-    public PredictRestController(PredictService predictService) {
+    public PredictRestController(PredictService predictService, StockService stockService) {
         this.predictService = predictService;
+        this.stockService = stockService;
     }
+
+    @PostMapping
+    public ResponseEntity<String> predictAndSave(@RequestParam String symbol) {
+        try {
+            predictService.predictAndSave(symbol);
+            return ResponseEntity.ok("Prediction requested and saved for symbol: " + symbol);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error processing prediction for symbol: " + symbol);
+        }
+    }
+
 
     @GetMapping("/{symbol}/latest")
     public ResponseEntity<PredictResponseDTO> getLatestPrediction(@PathVariable String symbol) {

03ffa768b4319fcf86e1e16ef782e35f54595b4b
DanielOgnyanov <escape69@abv.bg>
2025-08-26 08:17:15 +0300

    find all method in stock init

--- src/main/java/com/daniel/stockpredictorml/web/StockRestController.java
+++ src/main/java/com/daniel/stockpredictorml/web/StockRestController.java
@@ -1,17 +1,32 @@
 package com.daniel.stockpredictorml.web;
 
 
+import com.daniel.stockpredictorml.models.entities.StockEntity;
 import com.daniel.stockpredictorml.service.StockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/stock")
+@RequestMapping("/api/stocks")
 public class StockRestController {
 
     private final StockService stockService;
 
     public StockRestController(StockService stockService) {
         this.stockService = stockService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StockEntity>> getAllStocks() {
+        List<StockEntity> stocks = stockService.getAllStocks();
+
+        if (stocks.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(stocks);
     }
 }

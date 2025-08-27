eb602d02b52e159d4b4f7164e594a1305f8c16d8
DanielOgnyanov <escape69@abv.bg>
2025-08-27 09:28:35 +0300

    StockRestControllerTest  method testPredictStock

--- src/main/test/StockRestControllerTest.java
+++ src/main/test/StockRestControllerTest.java
@@ -1,6 +1,36 @@
+import com.daniel.stockpredictorml.service.PredictService;
 import com.daniel.stockpredictorml.web.StockRestController;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(StockRestController.class)
 public class StockRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockBean
+    private PredictService predictService;
+
+    @Test
+    void testPredictStock() throws Exception {
+        String symbol = "AAPL";
+
+
+        Mockito.doNothing().when(predictService).predictAndSave(symbol);
+
+        mockMvc.perform(post("/api/stocks/predict")
+                        .param("symbol", symbol))
+                .andExpect(status().isOk());
+    }
+
+
 }

9d39cc388883803e3170693bce73110b7ff82953
DanielOgnyanov <escape69@abv.bg>
2025-08-27 09:25:55 +0300

    StockRestControllerTest init

---
+++ src/main/test/StockRestControllerTest.java
@@ -0,0 +1,6 @@
+import com.daniel.stockpredictorml.web.StockRestController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(StockRestController.class)
+public class StockRestControllerTest {
+}

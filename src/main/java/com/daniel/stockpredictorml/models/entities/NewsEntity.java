5b8b7f5427a29977b4513252970298e213ac5987
DanielOgnyanov <escape69@abv.bg>
2025-09-04 13:08:28 +0300

    news entity init

---
+++ src/main/java/com/daniel/stockpredictorml/models/entities/NewsEntity.java
@@ -0,0 +1,17 @@
+package com.daniel.stockpredictorml.models.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class NewsEntity {
+}

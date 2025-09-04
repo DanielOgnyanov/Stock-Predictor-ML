78d15983957e54a31e6f74b92b56b1a1c6be4408
DanielOgnyanov <escape69@abv.bg>
2025-09-04 13:23:01 +0300

    news entity add validations

--- src/main/java/com/daniel/stockpredictorml/models/entities/NewsEntity.java
+++ src/main/java/com/daniel/stockpredictorml/models/entities/NewsEntity.java
@@ -1,8 +1,11 @@
 package com.daniel.stockpredictorml.models.entities;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -15,9 +18,23 @@ import lombok.*;
 @Data
 public class NewsEntity {
 
+    @NotBlank(message = "Title is required")
+    @Size(max = 255, message = "Title cannot exceed 255 characters")
+    @Column(nullable = false, length = 255)
     private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 1000, message = "Description cannot exceed 1000 characters")
+    @Column(nullable = false)
     private String description;
+
+    @Size(max = 500, message = "Snippet cannot exceed 500 characters")
+    @Column(length = 500)
     private String snippet;
+
+    @NotBlank(message = "Symbol is required")
+    @Size(max = 20, message = "Symbol cannot exceed 20 characters")
+    @Column(nullable = false, length = 20)
     private String symbol;
 
 }

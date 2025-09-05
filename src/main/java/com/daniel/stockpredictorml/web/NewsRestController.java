d13ec6845406a15aa0cbea2c80946eb0c9dea709
DanielOgnyanov <escape69@abv.bg>
2025-09-05 08:31:16 +0300

    news rest controller init

---
+++ src/main/java/com/daniel/stockpredictorml/web/NewsRestController.java
@@ -0,0 +1,10 @@
+package com.daniel.stockpredictorml.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsRestController {
+}

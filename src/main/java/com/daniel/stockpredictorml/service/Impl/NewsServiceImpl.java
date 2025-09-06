4df6a4f82f939915ed6bee8daf82d6aa859515eb
DanielOgnyanov <escape69@abv.bg>
2025-09-06 08:28:08 +0300

    NewsResponseDTO set

--- src/main/java/com/daniel/stockpredictorml/service/Impl/NewsServiceImpl.java
+++ src/main/java/com/daniel/stockpredictorml/service/Impl/NewsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.daniel.stockpredictorml.service.Impl;
 
+import com.daniel.stockpredictorml.models.dto.NewsResponseDTO;
 import com.daniel.stockpredictorml.repository.NewsRepositories;
 import com.daniel.stockpredictorml.service.NewsService;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,10 @@ public class NewsServiceImpl implements NewsService {
     private final RestTemplate restTemplate = new RestTemplate();
 
 
+
     private static final String API_KEY = "";
     private static final String BASE_URL = "https://api.marketaux.com/v1/news/all?symbols=TSLA,AMZN,MSFT&filter_entities=true&language=en&api_token=";
+    private static String url = BASE_URL + API_KEY;
 
     public NewsServiceImpl(NewsRepositories newsRepositories) {
         this.newsRepositories = newsRepositories;
@@ -23,6 +26,14 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public void fetchAndStoreNews() {
+
+        try{
+
+            NewsResponseDTO response = restTemplate.getForObject(url, NewsResponseDTO.class);
+
+        } catch (Exception e) {
+
+        }
 
     }
 }

64e5c7eb17d186eb05ef073eec9c2753690f7eb7
DanielOgnyanov <escape69@abv.bg>
2025-10-12 10:00:14 +0300

    create q query that return for every stock symbol the latest price

--- src/main/java/com/daniel/stockpredictorml/repository/StockRepository.java
+++ src/main/java/com/daniel/stockpredictorml/repository/StockRepository.java
@@ -5,6 +5,7 @@ package com.daniel.stockpredictorml.repository;
 
 import com.daniel.stockpredictorml.models.entities.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +16,11 @@ public interface StockRepository  extends JpaRepository<StockEntity, Long> {
 
     Optional<StockEntity> findTopBySymbolOrderByUpdatedAtDesc(String symbol);
 
-    List<StockEntity> findAll();
+    @Query("""
+    SELECT s FROM StockEntity s
+    WHERE s.updatedAt = (
+        SELECT MAX(s2.updatedAt)
+        FROM StockEntity s2
+        WHERE s2.symbol = s.symbol)""")
+    List<StockEntity> findLatestPricesPerSymbol();
 }

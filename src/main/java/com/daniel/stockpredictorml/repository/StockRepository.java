e13c485fbf921ed87993b8dbda082cb791361bbb
DanielOgnyanov <escape69@abv.bg>
2025-10-15 13:56:11 +0300

    create an query that extract the open price list ordered per date for every symbol.

--- src/main/java/com/daniel/stockpredictorml/repository/StockRepository.java
+++ src/main/java/com/daniel/stockpredictorml/repository/StockRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -23,4 +24,21 @@ public interface StockRepository  extends JpaRepository<StockEntity, Long> {
         FROM StockEntity s2
         WHERE s2.symbol = s.symbol)""")
     List<StockEntity> findLatestPricesPerSymbol();
+
+
+    @Query(value = """
+        SELECT 
+            symbol,
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'date', created_at,
+                    'open', open
+                )
+                ORDER BY created_at
+            ) AS open_prices
+        FROM stocks
+        GROUP BY symbol
+        ORDER BY symbol;
+    """, nativeQuery = true)
+    List<Map<String, Object>> findAllSymbolsWithOpenPriceHistory();
 }

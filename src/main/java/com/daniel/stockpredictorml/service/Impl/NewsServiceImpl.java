c23d831c1bf8397893d6a4b4e0f733cfce0df2b0
DanielOgnyanov <escape69@abv.bg>
2025-10-19 16:38:13 +0300

    update the news service to handle the empty description when the external api dosent have it

--- src/main/java/com/daniel/stockpredictorml/service/Impl/NewsServiceImpl.java
+++ src/main/java/com/daniel/stockpredictorml/service/Impl/NewsServiceImpl.java
@@ -30,23 +30,18 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public void fetchAndStoreNews() {
-
         try {
-
             String response = restTemplate.getForObject(url, String.class);
 
             if (response != null) {
-
                 ObjectMapper mapper = new ObjectMapper();
                 JsonNode root = mapper.readTree(response);
-
                 JsonNode dataArray = root.path("data");
 
                 for (JsonNode newsNode : dataArray) {
-
-                    String title = newsNode.path("title").asText(null);
-                    String description = newsNode.path("description").asText(null);
-                    String snippet = newsNode.path("snippet").asText(null);
+                    String title = newsNode.path("title").asText("");
+                    String description = newsNode.path("description").asText("");
+                    String snippet = newsNode.path("snippet").asText("");
 
                     JsonNode entities = newsNode.path("entities");
                     String symbol = null;
@@ -54,28 +49,24 @@ public class NewsServiceImpl implements NewsService {
                         symbol = entities.get(0).path("symbol").asText(null);
                     }
 
+                    // Skip invalid or incomplete news
+                    if (title.isBlank() && description.isBlank()) continue;
 
                     NewsEntity newsEntity = NewsEntity.builder()
-
-                            .title(title)
-                            .description(description)
-                            .snippet(snippet)
+                            .title(title.isBlank() ? "No title available" : title)
+                            .description(description.isBlank() ? "No description available" : description)
+                            .snippet(snippet.isBlank() ? "No snippet available" : snippet)
                             .symbol(symbol)
                             .build();
 
                     newsRepositories.save(newsEntity);
-
                 }
-
-
             }
-
         } catch (Exception e) {
             System.err.println("Error fetching/saving symbol: " + e.getMessage());
         }
-
-
     }
+
 
 
     @Override
